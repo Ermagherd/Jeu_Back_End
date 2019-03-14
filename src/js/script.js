31a6b898e5af069efa583da331d623a58987847d
Ermagherd <39029837+Ermagherd@users.noreply.github.com>
2019-03-14 23:45:10 +0100

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,6 @@ window.addEventListener('DOMContentLoaded', function () {
 
   // lancement web socket
   var socket = io("http://www.localhost:8888");
-  
-  // verif des idenfiants de connexion
-  $('#sendMessageButton').submit(function (e) {
-    e.preventDefault();
-    console.log('in');
-  })
 
 	///////////////////////////////////////////////////////////////
 	/////                     LANCEMENT DU JEU                /////
