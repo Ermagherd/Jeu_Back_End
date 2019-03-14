4f45e9e7ca82bc5d7f85446b47535c4388e26368
Ermagherd <39029837+Ermagherd@users.noreply.github.com>
2019-03-14 23:45:01 +0100

    Create login.js

---
+++ src/js/login.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  $('#name').focus(function (e) {
+    $('#name').parent().children('p').hide()
+  })
+  $('#pwd').focus(function (e) {
+    $('#pwd').parent().children('p').hide()
+  })
+  $('#pwd-check').focus(function (e) {
+    $('#pwd-check').parent().children('p').hide()
+  })
+  
+
+  // verif des idenfiants de connexion
+  $('#connexionForm').submit(function (e) {
+    e.preventDefault();
+    var name = $('#name').val();
+    var pwd = $('#pwd').val();
+    var confPwd = $('#pwd-check').val();
+
+    if (name === ''){
+      $('#name').parent().children('p').html('Veuillez renseigner un pseudonyme.')
+      $('#name').parent().children('p').show()
+    }
+    if (pwd === ''){
+      $('#pwd').parent().children('p').html('Veuillez renseigner un password.')
+      $('#pwd').parent().children('p').show()
+    }
+    if (pwd !== confPwd){
+      $('#pwd-check').parent().children('p').html('Les 2 mots de passe ne sont pas identiques')
+      $('#pwd-check').parent().children('p').show()
+    }
+
+    if ('' !== name && '' !== pwd && '' !== confPwd && pwd === confPwd){
+      $.get("/accueil", function (data){
+        
+      })
+    }
+
+  })
+
+}, true);
